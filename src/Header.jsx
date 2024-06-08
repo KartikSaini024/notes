4319c178bbb033e96697531fa54b76350c83605a
Kartik Saini <113547067+KartikSaini024@users.noreply.github.com>
2024-06-08 17:40:28 +0530

    Update Header.jsx
    Changed login status

--- src/Header.jsx
+++ src/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
         <>
             <div className='header'>
                 <img src={logo} className='logo-style' alt="My Notes Logo" />
-                <UserGreet isLoggedIn={true} username="Admin" />
+                <UserGreet isLoggedIn={false} username="Guest" />
             </div>
         </>
     )
